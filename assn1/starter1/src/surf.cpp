5a8f200ec6e19eb2a249ee1e8f68e4d3820a70b8
Svetlozar S <stormcrafterbg@gmail.com>
2022-08-31 17:28:48 +0300

    Implemented Surfaces of Revolution

--- assn1/starter1/src/surf.cpp
+++ assn1/starter1/src/surf.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 namespace
 {
-    const float c_pi = 3.14159265358979323846f;
-    // We're only implenting swept surfaces where the profile curve is
-    // flat on the xy-plane.  This is a check function.
-    static bool checkFlat(const Curve &profile)
-    {
-        for (unsigned i=0; i<profile.size(); i++)
-            if (profile[i].V[2] != 0.0 ||
-                profile[i].T[2] != 0.0 ||
-                profile[i].N[2] != 0.0)
-                return false;
-    
-        return true;
-    }
+	const float c_pi = 3.14159265358979323846f;
+	// We're only implenting swept surfaces where the profile curve is
+	// flat on the xy-plane.  This is a check function.
+	static bool checkFlat(const Curve& profile)
+	{
+		for (unsigned i = 0; i < profile.size(); i++)
+			if (profile[i].V[2] != 0.0 ||
+				profile[i].T[2] != 0.0 ||
+				profile[i].N[2] != 0.0)
+				return false;
+
+		return true;
+	}
 }
 
 // DEBUG HELPER
-Surface quad() { 
+Surface quad() {
 	Surface ret;
 	ret.VV.push_back(Vector3f(-1, -1, 0));
 	ret.VV.push_back(Vector3f(+1, -1, 0));
@@ -37,110 +37,139 @@ Surface quad() {
 	return ret;
 }
 
-Surface makeSurfRev(const Curve &profile, unsigned steps)
+Surface makeSurfRev(const Curve& profile, unsigned steps)
 {
-    Surface surface;
+	Surface surface;
 	//surface = quad();
-    
-    if (!checkFlat(profile))
-    {
-        cerr << "surfRev profile curve must be flat on xy plane." << endl;
-        exit(0);
-    }
 
-    float rotStep = (360.0f / steps);
-    float rotDeg = rotStep;
+	if (!checkFlat(profile))
+	{
+		cerr << "surfRev profile curve must be flat on xy plane." << endl;
+		exit(0);
+	}
 
-    for (int n = 0; n < profile.size(); n++)
-    {
-           Vector4f vec = Matrix4f(Vector4f(1, 0, 0, 0),
-            Vector4f(0, 1, 0, 0),
-            Vector4f(0, 0, 1, 0),
-            Vector4f(profile[n].V.xyz(), 1)) *
-            Matrix4f::rotateY(2 * c_pi * (rotDeg / 360.0f)) * 
-            Matrix4f(Vector4f(1, 0, 0, 0),
-            Vector4f(0, 1, 0, 0),
-            Vector4f(0, 0, 1, 0),
-            Vector4f(-profile[n].V.xyz(), 1)) * Vector4f(profile[n].V.x(), 0, profile[n].V.z(), 1);
+	int profDotCount = (int)profile.size();
 
-        surface.VV.push_back(vec.xyz());
-        rotDeg += rotStep;
+	for (unsigned s = 0; s <= steps; s++)
+	{
+		Matrix3f rotMatrix = Matrix3f::rotateY(2 * c_pi * (float(s) / steps));
+		Matrix3f nRotMatrix = rotMatrix.inverse().transposed();
+		for (int n = 0; n < profDotCount; n++)
+		{
+			Vector4f vec = rotMatrix * profile[n].V;
 
-        surface.VF.push_back(Tup3u(0, 1, 2));
-        surface.VN.push_back(Vector3f(0, 0, 1));
-    }
-    
+			surface.VV.push_back(vec.xyz());
 
-    //// TODO: Here you should build the surface.  See surf.h for details.
+			surface.VN.push_back(-(nRotMatrix * profile[n].N).normalized());
+		}
 
-    //cerr << "\t>>> makeSurfRev called (but not implemented).\n\t>>> Returning empty surface." << endl;
- 
-    return surface;
+		if (s > 0)
+		{
+			for (int n = 0; n < profDotCount - 1; n++)
+			{
+				int p1 = (s * profDotCount) + n;
+
+				int p2 = (s - 1) * profDotCount + n;
+				int p3 = (s - 1) * profDotCount + n + 1;
+
+				surface.VF.push_back(Tup3u(p1, p2, p3));
+
+				p2 = (s - 1) * profDotCount + n + 1;
+				p3 = s * profDotCount + n + 1;
+
+				surface.VF.push_back(Tup3u(p1, p2, p3));
+			}
+		}
+	}
+
+
+	/*for (int i = 0; i < 30; i++)
+	{
+		for (unsigned j = 1; j <= steps; j++)
+		{
+			int p1 = i * steps + j;
+
+			int p2;
+			int p3;
+
+			p2 = p1 - 1;
+			p3 = (1 + i) * steps + p2;
+
+			surface.VF.push_back(Tup3u(p1, p2, p3));
+
+			p2 = (1 + i) * steps + j - 1;
+			p3 = (1 + i) * steps + p1;
+
+			surface.VF.push_back(Tup3u(p1, p2, p3));
+		}
+	}*/
+
+	return surface;
 }
 
-Surface makeGenCyl(const Curve &profile, const Curve &sweep )
+Surface makeGenCyl(const Curve& profile, const Curve& sweep)
 {
-    Surface surface;
+	Surface surface;
 	surface = quad();
 
-    if (!checkFlat(profile))
-    {
-        cerr << "genCyl profile curve must be flat on xy plane." << endl;
-        exit(0);
-    }
+	if (!checkFlat(profile))
+	{
+		cerr << "genCyl profile curve must be flat on xy plane." << endl;
+		exit(0);
+	}
 
-    // TODO: Here you should build the surface.  See surf.h for details.
+	// TODO: Here you should build the surface.  See surf.h for details.
 
-    cerr << "\t>>> makeGenCyl called (but not implemented).\n\t>>> Returning empty surface." <<endl;
+	cerr << "\t>>> makeGenCyl called (but not implemented).\n\t>>> Returning empty surface." << endl;
 
-    return surface;
+	return surface;
 }
 
-void recordSurface(const Surface &surface, VertexRecorder* recorder) {
+void recordSurface(const Surface& surface, VertexRecorder* recorder) {
 	const Vector3f WIRECOLOR(0.4f, 0.4f, 0.4f);
-    for (int i=0; i<(int)surface.VF.size(); i++)
-    {
+	for (int i = 0; i < (int)surface.VF.size(); i++)
+	{
 		recorder->record(surface.VV[surface.VF[i][0]], surface.VN[surface.VF[i][0]], WIRECOLOR);
 		recorder->record(surface.VV[surface.VF[i][1]], surface.VN[surface.VF[i][1]], WIRECOLOR);
 		recorder->record(surface.VV[surface.VF[i][2]], surface.VN[surface.VF[i][2]], WIRECOLOR);
-    }
+	}
 }
 
-void recordNormals(const Surface &surface, VertexRecorder* recorder, float len)
+void recordNormals(const Surface& surface, VertexRecorder* recorder, float len)
 {
 	const Vector3f NORMALCOLOR(0, 1, 1);
-    for (int i=0; i<(int)surface.VV.size(); i++)
-    {
+	for (int i = 0; i < (int)surface.VV.size(); i++)
+	{
 		recorder->record_poscolor(surface.VV[i], NORMALCOLOR);
 		recorder->record_poscolor(surface.VV[i] + surface.VN[i] * len, NORMALCOLOR);
-    }
+	}
 }
 
-void outputObjFile(ostream &out, const Surface &surface)
+void outputObjFile(ostream& out, const Surface& surface)
 {
-    
-    for (int i=0; i<(int)surface.VV.size(); i++)
-        out << "v  "
-            << surface.VV[i][0] << " "
-            << surface.VV[i][1] << " "
-            << surface.VV[i][2] << endl;
 
-    for (int i=0; i<(int)surface.VN.size(); i++)
-        out << "vn "
-            << surface.VN[i][0] << " "
-            << surface.VN[i][1] << " "
-            << surface.VN[i][2] << endl;
+	for (int i = 0; i < (int)surface.VV.size(); i++)
+		out << "v  "
+		<< surface.VV[i][0] << " "
+		<< surface.VV[i][1] << " "
+		<< surface.VV[i][2] << endl;
 
-    out << "vt  0 0 0" << endl;
-    
-    for (int i=0; i<(int)surface.VF.size(); i++)
-    {
-        out << "f  ";
-        for (unsigned j=0; j<3; j++)
-        {
-            unsigned a = surface.VF[i][j]+1;
-            out << a << "/" << "1" << "/" << a << " ";
-        }
-        out << endl;
-    }
+	for (int i = 0; i < (int)surface.VN.size(); i++)
+		out << "vn "
+		<< surface.VN[i][0] << " "
+		<< surface.VN[i][1] << " "
+		<< surface.VN[i][2] << endl;
+
+	out << "vt  0 0 0" << endl;
+
+	for (int i = 0; i < (int)surface.VF.size(); i++)
+	{
+		out << "f  ";
+		for (unsigned j = 0; j < 3; j++)
+		{
+			unsigned a = surface.VF[i][j] + 1;
+			out << a << "/" << "1" << "/" << a << " ";
+		}
+		out << endl;
+	}
 }
